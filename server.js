c150069632a7dac65b9748f38e6266d1254b3d13
adityadeval <31914044+adityadeval@users.noreply.github.com>
2017-09-19 07:45:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ function createTemplate(content)
         `<html>
             <head>
                 <title>
-                     $title
+                     ${title}
                 </title>
                 <meta name="viewport" content="width=device-width,initial-scale=1"/>
                 <link href="/ui/style.css" rel="stylesheet" />
@@ -90,13 +90,13 @@ function createTemplate(content)
                     </div>
                     <hr/>
                     <h3>
-                        $heading
+                        ${heading}
                     </h3>
                     <div>
-                        $date
+                        ${date}
                     </div>
                     <div>
-                        $data
+                        ${data}
                     </div>
                 </div>
             </body>
