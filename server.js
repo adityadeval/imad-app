e5c8f5443fc8f9764c4773eb4614b86558fff499
adityadeval <31914044+adityadeval@users.noreply.github.com>
2017-09-19 23:11:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(morgan('combined'));
 
 //Articles=
 //{
-    articleone={"title-one":"Article One|Aditya Deval",
+    articleone={"title":"Article One|Aditya Deval",
     "heading":"Article One",
     "date":"19 Sept 2017",
     "data":`<p>
@@ -97,7 +97,7 @@ function createTemplate(content)
 }
 
 app.get('/', function (req, res) {
-    res.send(articleone.title-one);
+    res.send(articleone.title);
 });
 //app.get('/', function (req, res) {
 //  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
