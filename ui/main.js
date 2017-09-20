1dbb3735e29e834d528759d61503235e8771ee15
adityadeval <31914044+adityadeval@users.noreply.github.com>
2017-09-20 16:49:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded main.js!');
 //alert("Hi(2)");
 //var element=document.getElementById("divForJs");
 //element.innerHTML="Changed original using javascript !";
+var element2=document.getElementById("divForMovingImgbyJs");
+            element2.onclick=function(){
+                element2.style.marginLeft='100px';
+            };
