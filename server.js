18f9409a07cb2d159ead1479c00435681e6537ce
adityadeval <31914044+adityadeval@users.noreply.github.com>
2017-09-21 10:47:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.get('/', function (req, res) {
 var counter=0;
 app.get('/counter', function (req, res) {
   counter=counter+1;
-  res.send(counter.toString);
+  res.send("c");
 });
 
 //app.get('/articleone',function(req,res){
